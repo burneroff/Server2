ddb61cd33f603ab5e157661d41bba55f7c24d429
Burner <128158213+burneroff@users.noreply.github.com>
2024-10-12 19:39:58 +0300

    finall

--- src/index.ts
+++ src/index.ts
@@ -10,20 +10,26 @@ import { Bids } from "./entities/Bids";
 
 const startServer = async () => {
   try {
-    await AppDataSource.initialize().then(() => {
-      console.log("Data Source has been initialized!")
-    })
+    await AppDataSource.initialize();
+    console.log("Data Source has been initialized!");
 
     const app = express();
     app.use(express.json());
     app.use(cookieParser());
+    
     app.use(cors({
-      origin: '*',
-      credentials: true,
+      origin: 'https://locatorcost.vercel.app', // Укажите разрешенный источник
+      credentials: true, // Разрешите учетные данные
     }));
+
     app.use("/api/auth", routerAuth);
     app.use("/api/object", routerObject);
     app.use("/api/price", routerPrice);
+
+    // Простой маршрут для проверки состояния
+    app.get('/health', (req, res) => {
+      res.send('OK');
+    });
 
     app.listen(5000, "0.0.0.0", () => {
       console.log("Connected on port 5000!");
@@ -32,50 +38,34 @@ const startServer = async () => {
     const updateAuction = async () => {
       const BidsRepository = AppDataSource.getRepository(Bids);
       let bids = await BidsRepository.find();
-      // Проходим по всем записям
-      for (let bid of bids) {
-        // Проверяем, если dateForChangePrice меньше текущей даты
-        if (bid.status == "Торги идут"){
-        if (new Date(bid.dateForChangePrice) < new Date()) {
-          // Приведение значений к числу
-          const nowPrice = parseFloat(bid.nowPrice as unknown as string);
-          const step = parseFloat(bid.step as unknown as string);
-          const minimalPrice = parseFloat(bid.minimalPrice as unknown as string);
-    
-          // Вычисляем новое значение nowPrice
-          const newPrice = nowPrice - step;
-    
-          // Проверяем, если новое значение nowPrice становится равным minimalPrice
-          if (newPrice <= minimalPrice && bid.numberOfBets > 0) {
-            // Обновляем поле nowPrice и status
-            bid.nowPrice = newPrice;
-            bid.status = "Торги завершены успешно";
-          } else {
-            // Если не достигнуто минимальное значение или нет ставок, просто обновляем поле nowPrice
-            bid.nowPrice = newPrice;
-            console.log("Изменили цену!")
-          }
 
-          
-        }
-          // Сохраняем обновленную запись
+      for (let bid of bids) {
+        if (bid.status == "Торги идут") {
+          if (new Date(bid.dateForChangePrice) < new Date()) {
+            const nowPrice = parseFloat(bid.nowPrice as unknown as string);
+            const step = parseFloat(bid.step as unknown as string);
+            const minimalPrice = parseFloat(bid.minimalPrice as unknown as string);
+            const newPrice = nowPrice - step;
+
+            if (newPrice <= minimalPrice && bid.numberOfBets > 0) {
+              bid.nowPrice = newPrice;
+              bid.status = "Торги завершены успешно";
+            } else {
+              bid.nowPrice = newPrice;
+              console.log("Изменили цену!");
+            }
+          }
           await BidsRepository.save(bid);
         }
       }
     };
     
-    const job = CronJob.from({
-      cronTime: '*/5 * * * *',
-      onTick: function (){ updateAuction()},
-      start: true,
-      timeZone: 'Europe/Minsk'
-    });
+    const job = new CronJob('*/5 * * * *', updateAuction, null, true, 'Europe/Minsk');
 
   } catch (err) {
     console.error("Error during Server initialization:", err);
   }
 };
-
 
 startServer();
 
