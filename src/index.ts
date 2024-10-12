a33e50aa102ba058d5529f016454368c8a0697e4
Burner <128158213+burneroff@users.noreply.github.com>
2024-10-12 19:17:33 +0300

    аа

--- src/index.ts
+++ src/index.ts
@@ -7,72 +7,75 @@ import { routerObject } from "./routes/routerObject";
 import { routerPrice } from "./routes/routerPrice";
 import { CronJob } from "cron";
 import { Bids } from "./entities/Bids";
-import https from 'https';
-import fs from 'fs';
-import path from 'path';
 
 const startServer = async () => {
   try {
     await AppDataSource.initialize().then(() => {
-      console.log("Data Source has been initialized!");
-    });
+      console.log("Data Source has been initialized!")
+    })
 
     const app = express();
     app.use(express.json());
     app.use(cookieParser());
     app.use(cors({
       origin: '*',
+      credentials: true,
     }));
-
     app.use("/api/auth", routerAuth);
     app.use("/api/object", routerObject);
     app.use("/api/price", routerPrice);
 
-    // Настройки для SSL
-    const sslOptions = {
-      key: fs.readFileSync(path.join(__dirname, 'server.key')),   // путь к приватному ключу
-      cert: fs.readFileSync(path.join(__dirname, 'server.cert')), // путь к сертификату
-    };
-
-    // Запуск HTTPS сервера
-    https.createServer(sslOptions, app).listen(443, () => {
-      console.log("Connected with HTTPS on port 443!");
+    app.listen(5000, () => {
+      console.log("Connected!");
     });
 
     const updateAuction = async () => {
       const BidsRepository = AppDataSource.getRepository(Bids);
       let bids = await BidsRepository.find();
-
+      // Проходим по всем записям
       for (let bid of bids) {
-        if (bid.status == "Торги идут") {
-          if (new Date(bid.dateForChangePrice) < new Date()) {
-            const nowPrice = parseFloat(bid.nowPrice as unknown as string);
-            const step = parseFloat(bid.step as unknown as string);
-            const minimalPrice = parseFloat(bid.minimalPrice as unknown as string);
-
-            const newPrice = nowPrice - step;
-
-            if (newPrice <= minimalPrice && bid.numberOfBets > 0) {
-              bid.nowPrice = newPrice;
-              bid.status = "Торги завершены успешно";
-            } else {
-              bid.nowPrice = newPrice;
-              console.log("Изменили цену!");
-            }
+        // Проверяем, если dateForChangePrice меньше текущей даты
+        if (bid.status == "Торги идут"){
+        if (new Date(bid.dateForChangePrice) < new Date()) {
+          // Приведение значений к числу
+          const nowPrice = parseFloat(bid.nowPrice as unknown as string);
+          const step = parseFloat(bid.step as unknown as string);
+          const minimalPrice = parseFloat(bid.minimalPrice as unknown as string);
+    
+          // Вычисляем новое значение nowPrice
+          const newPrice = nowPrice - step;
+    
+          // Проверяем, если новое значение nowPrice становится равным minimalPrice
+          if (newPrice <= minimalPrice && bid.numberOfBets > 0) {
+            // Обновляем поле nowPrice и status
+            bid.nowPrice = newPrice;
+            bid.status = "Торги завершены успешно";
+          } else {
+            // Если не достигнуто минимальное значение или нет ставок, просто обновляем поле nowPrice
+            bid.nowPrice = newPrice;
+            console.log("Изменили цену!")
           }
+
+          
+        }
+          // Сохраняем обновленную запись
           await BidsRepository.save(bid);
-          console.log(`Updated bid: ${bid.id}`);
         }
       }
     };
-
-    // Cron job для обновления аукционов каждые 5 минут
-    const job = new CronJob('*/5 * * * *', updateAuction, null, true, 'Europe/Minsk');
+    
+    const job = CronJob.from({
+      cronTime: '*/5 * * * *',
+      onTick: function (){ updateAuction()},
+      start: true,
+      timeZone: 'Europe/Minsk'
+    });
 
   } catch (err) {
     console.error("Error during Server initialization:", err);
   }
 };
+
 
 startServer();
 
