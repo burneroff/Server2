6d96686e56a455caf11f95c76ea4e9236f2757cd
Burner <128158213+burneroff@users.noreply.github.com>
2024-10-12 16:47:05 +0300

    sdas

--- src/routes/routerPrice.ts
+++ src/routes/routerPrice.ts
@@ -3,5 +3,5 @@ import * as PriceController from "../controllers/PriceController"
 export const routerPrice = express.Router();
  
 routerPrice.get("/getNowPrice/:id", PriceController.getPrice);
-routerPrice.post("/setPrice", PriceController.setPrice);
+routerPrice.get("/setPrice", PriceController.setPrice);
 routerPrice.get("/test", PriceController.setPrice);
